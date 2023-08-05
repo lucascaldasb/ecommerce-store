3889746a6b8ce0a0d35aa2fb2ed5e983636cf875
lucascaldasb <lucas20caldas@gmail.com>
2023-08-05 12:39:01 -0300

    creating products list and product card
    Signed-off-by: lucascaldasb <lucas20caldas@gmail.com>

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,23 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
 
-export default function HomePage() {
+export const revalidate = 0
+
+export default async function HomePage() {
+  const products = await getProducts({ isFeatured: true})
+  const billboard = await getBillboard('eb1b283c-d814-4eed-8d45-75a442fb7fb9')
+
   return (
-    <div>
-      Hello Store
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard}/>
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
   )
 }
