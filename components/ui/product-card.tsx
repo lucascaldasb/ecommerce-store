b9c2de2b274ab0b6e560d5a7e405346299e033b7
lucascaldasb <lucas20caldas@gmail.com>
2023-08-05 19:40:35 -0300

    creating cart page
    Signed-off-by: lucascaldasb <lucas20caldas@gmail.com>

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,6 +8,7 @@ import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCardProps {
   data: Product
@@ -16,6 +17,7 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({
   data
 }) => {
+  const cart = useCart()
   const previewModal = usePreviewModal()
   const router = useRouter()
 
@@ -27,6 +29,12 @@ const ProductCard: React.FC<ProductCardProps> = ({
     event.stopPropagation()
 
     previewModal.onOpen(data)
+  }
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation()
+
+    cart.addItem(data)
   }
 
   return (
@@ -46,7 +54,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
             />
             <IconButton 
               icon={<ShoppingCart size={20} className="text-gray-600"/>}
-              onClick={() => {}}
+              onClick={onAddToCart}
             />
           </div>
         </div>
